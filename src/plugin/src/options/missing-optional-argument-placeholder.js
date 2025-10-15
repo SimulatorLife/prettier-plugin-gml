c45803f5a6b94486d6a9c9a0211305ec09b14c2e
Henry Kirk <henry@the-kirks.com>
2025-10-15 09:44:21 -0400

    Simplified optional arg placeholder option

--- src/plugin/src/options/missing-optional-argument-placeholder.js
+++ src/plugin/src/options/missing-optional-argument-placeholder.js
@@ -1,9 +1,4 @@
-const MissingOptionalArgumentPlaceholder = Object.freeze({
+export const MissingOptionalArgumentPlaceholder = Object.freeze({
     UNDEFINED: "undefined",
     EMPTY: "empty"
 });
-
-export const DEFAULT_MISSING_OPTIONAL_ARGUMENT_PLACEHOLDER =
-    MissingOptionalArgumentPlaceholder.UNDEFINED;
-
-export { MissingOptionalArgumentPlaceholder };
